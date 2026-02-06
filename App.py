8c3b35dfa51460cac4a17dfe67d268b9273c01ac
mithunsabari07 <mithunsabari17@gmail.com>
2026-02-06 11:30:54 +0530

    Use environment variable for port configuration

--- App.py
+++ App.py
@@ -615,5 +615,8 @@ def get_time_ago(timestamp):
 with app.app_context():
     init_db()
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
